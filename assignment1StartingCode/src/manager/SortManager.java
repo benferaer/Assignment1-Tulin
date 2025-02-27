fde66e490e4ec0a65c01152da9809f3292023066
benferaer <benferaer@gmail.com>
2025-02-27 15:48:27 -0700

    loadShapes method done

--- assignment1StartingCode/src/manager/SortManager.java
+++ assignment1StartingCode/src/manager/SortManager.java
@@ -1,6 +1,17 @@
 package manager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import shapes.Cone;
+import shapes.Cylinder;
+import shapes.OctagonalPrism;
+import shapes.PentagonalPrism;
+import shapes.Pyramid;
 import shapes.Shape;
+import shapes.SquarePrism;
+import shapes.TriangularPrism;
 
 //refer to demo001 BasicFileIO.java for a simple example on how to
 		// read data from a text file
@@ -30,23 +41,103 @@ public class SortManager
 			{
 				fileName = s.substring(2);
 			}
-			else if (s.startsWith("-t") || s.startsWith("-T"))
-			{
-				compareType = s.charAt(2);
-			}
-			else if (s.startsWith("-s") || s.startsWith("-S"))
-			{
-				sortType = s.charAt(2);
-			}
+//			else if (s.startsWith("-t") || s.startsWith("-T"))
+//			{
+//				compareType = s.charAt(2);
+//			}
+//			else if (s.startsWith("-s") || s.startsWith("-S"))
+//			{
+//				sortType = s.charAt(2);
+//			}
 		}
 		
 		loadShapes();
+		//placeholder to print shapes for testing loadShapes
+		printShapes();
 		
 	}
 	
+	/**
+	 * Method for loading the Shapes array from the file name define in args
+	 */
 	private void loadShapes()
+    {
+	File file = new File("res/"+fileName);
+	try
 	{
-		//from fileName, load shapes
-		//first line is the length of the array
+	    Scanner input = new Scanner(file);
+	    //Taking the first line as the array length 
+	    int arrLength = Integer.parseInt(input.nextLine().trim());
+	    
+	    shapes = new Shape[arrLength];
+	    int index = 0;
+	    
+	    for(int i = 0; i < arrLength; i++)
+	    {
+	    	if(input.hasNextLine()) 
+	    	{
+	    		String line = input.nextLine();
+	    		String[] parts = line.split("\\s+");
+	    		
+	    		switch (parts[0].toLowerCase())
+	    		{
+	    		case "cone":
+	    			double height = Double.parseDouble(parts[1]);
+	    			double radius = Double.parseDouble(parts[2]);
+	    			
+	    			shapes[index++] = new Cone(height, radius);
+	    			break;
+	    		case "cylinder":
+	    			double cylHeight = Double.parseDouble(parts[1]);
+	    			double cylRadius = Double.parseDouble(parts[2]);
+	    			
+	    			shapes[index++] = new Cylinder(cylHeight, cylRadius);
+	    			break;
+	    		case "octagonalprism":
+	    			double opHeight = Double.parseDouble(parts[1]);
+	    			double opLength = Double.parseDouble(parts[2]);
+	    			
+	    			shapes[index++] = new OctagonalPrism(opHeight, opLength);
+	    			break;
+	    		case "pentagonalprism":
+	    			double ppHeight = Double.parseDouble(parts[1]);
+	    			double ppLength = Double.parseDouble(parts[2]);
+	    			
+	    			shapes[index++] = new PentagonalPrism(ppHeight, ppLength);
+	    			break;
+	    		case "pyramid":
+	    			double pHeight = Double.parseDouble(parts[1]);
+	    			double pLength = Double.parseDouble(parts[2]);
+	    			
+	    			shapes[index++] = new Pyramid(pHeight, pLength);
+	    			break;
+	    		case "squareprism":
+	    			double spHeight = Double.parseDouble(parts[1]);
+	    			double spLength = Double.parseDouble(parts[2]);
+	    			
+	    			shapes[index++] = new SquarePrism(spHeight, spLength);
+	    			break;
+	    		case "triangularprism":
+	    			double tpHeight = Double.parseDouble(parts[1]);
+	    			double tpLength = Double.parseDouble(parts[2]);
+	    			
+	    			shapes[index++] = new TriangularPrism(tpHeight, tpLength);
+	    			break;
+	    		}
+	    	}
+	    }
+	}
+	catch (FileNotFoundException e) 
+	{
+		e.printStackTrace();
+	}
+    }
+	
+	//placeholder method for printing shapes for checking
+	private void printShapes() {
+		for (Shape shape : shapes)
+		{
+			System.out.println(shape);
+		}
 	}
 }
