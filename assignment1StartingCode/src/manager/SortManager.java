3cb62cf0a661b5320ba8ea324db7940fefcb4b31
benferaer <benferaer@gmail.com>
2025-02-27 12:32:25 -0700

    Expanded starting code

---
+++ assignment1StartingCode/src/manager/SortManager.java
@@ -0,0 +1,46 @@
+package manager;
+
+import shapes.Shape;
+
+//refer to demo001 BasicFileIO.java for a simple example on how to
+		// read data from a text file
+
+		// refer to demo01 Test.java for an example on how to parse command
+		// line arguments and benchmarking tests
+
+		// refer to demo02 Student.java for comparable implementation, and
+		// NameCompare.java or GradeCompare for comparator implementations
+
+		// refer to demo02 KittySort.java on how to use a custom sorting
+		// algorithm on a list of comparables to sort using either the
+		// natural order (comparable) or other orders (comparators)
+public class SortManager 
+{
+	private Shape[] shapes;
+	private String fileName;
+	private char compareType;
+	private char sortType;
+	
+	public SortManager(String[] args)
+	{
+		for(String s: args)
+		{
+			System.out.println(s);
+			if(s.startsWith("-f") || s.startsWith("-F"))
+			{
+				fileName = s.substring(2);
+			}
+			//else if (s.startsWith t for comparetype
+			//else if (s.startWith s for sorttype
+		}
+		
+		loadShapes();
+		
+	}
+	
+	private void loadShapes()
+	{
+		//from fileName, load shapes
+		//first line is the length of the array
+	}
+}
--- assignment1StartingCode/src/shapes/Cone.java
+++ assignment1StartingCode/src/shapes/Cone.java
@@ -1,6 +1,39 @@
 package shapes;
-
-public class Cone
+/**
+ * Subclass of Shape representing a Cone with a radius
+ * @author Tulin
+ */
+public class Cone extends Shape
 {
+	private double radius;
 
+	public Cone(double height, double radius) 
+	{
+		super(height);
+		this.radius = radius;
+	}
+
+	public double getRadius() 
+	{
+		return radius;
+	}
+
+	@Override
+	public double calcBaseArea() 
+	{
+		return Math.PI * radius * radius;
+	}
+
+	@Override
+	public double calcVolume() 
+	{
+		return calcBaseArea() * height / 3;
+	}
+
+	@Override
+	public String toString() {
+		return "Cone [getRadius()=" + getRadius() + ", calcBaseArea()=" + calcBaseArea() + ", calcVolume()="
+				+ calcVolume() + ", getHeight()=" + getHeight() + "]";
+	}	
+	
 }
