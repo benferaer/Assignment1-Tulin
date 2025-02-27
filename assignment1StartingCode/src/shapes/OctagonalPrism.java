1ac7f1730de8505dc27cb957e7f8bd361505b7be
benferaer <benferaer@gmail.com>
2025-02-27 13:22:11 -0700

    Completed shape classes

--- assignment1StartingCode/src/shapes/OctagonalPrism.java
+++ assignment1StartingCode/src/shapes/OctagonalPrism.java
@@ -1,6 +1,40 @@
 package shapes;
 
-public class OctagonalPrism
-{
+public class OctagonalPrism extends Shape
+{	
+	private double length;
 
+	public OctagonalPrism(double height, double length) 
+	{
+		super(height);
+		this.length = length;
+	}
+
+	public double getLength() 
+	{
+		return length;
+	}
+
+	@Override
+	public double calcBaseArea() 
+	{
+		return 2 * (1 + Math.sqrt(2)) * (length * length);
+	}
+
+	@Override
+	public double calcVolume() 
+	{
+		return calcBaseArea() * height;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "OctagonalPrism [getLength()=" + getLength() + ", calcBaseArea()=" + calcBaseArea() + ", calcVolume()="
+				+ calcVolume() + ", getHeight()=" + getHeight() + "]";
+	}
+	
+	
+	
+	
 }
--- assignment1StartingCode/src/shapes/Pyramid.java
+++ assignment1StartingCode/src/shapes/Pyramid.java
@@ -1,6 +1,36 @@
 package shapes;
 
-public class Pyramid
+public class Pyramid extends Shape
 {
+	private double length;
+
+	public Pyramid(double height, double length) 
+	{
+		super(height);
+		this.length = length;
+	}
+
+	public double getLength() 
+	{
+		return length;
+	}
+
+	@Override
+	public double calcBaseArea() 
+	{
+		return length * length;
+	}
+
+	@Override
+	public double calcVolume() 
+	{
+		return calcBaseArea() * height / 3;
+	}
+
+	@Override
+	public String toString() {
+		return "Pyramid [getLength()=" + getLength() + ", calcBaseArea()=" + calcBaseArea() + ", calcVolume()="
+				+ calcVolume() + ", getHeight()=" + getHeight() + "]";
+	}
 
 }
